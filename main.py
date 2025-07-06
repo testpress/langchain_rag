6ae8ed8b2c25ecc5421b3f458485bcad59ac8699
Bharathwaaj S <bharath@testpress.in>
2025-07-06 11:44:33 +0000

    Refactor main.py to enhance question-answering capabilities. Introduced a new retrieval function and updated the state management with a graph structure. Removed deprecated classes and streamlined the process for generating responses using retrieved context. Added support for tool integration and improved message handling.

--- main.py
+++ main.py
@@ -7,22 +7,12 @@ from langchain import hub
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langgraph.graph import START, StateGraph
-from typing_extensions import List, TypedDict
-from IPython.display import Image, display
-from typing import Literal
-
-from typing_extensions import Annotated
-
-class Search(TypedDict):
-    """Search query."""
-    query: Annotated[str, ..., "Search query to run."]
-    section: Annotated[
-        Literal["beginning", "middle", "end"],
-        ...,
-        "Section to query.",
-    ]
-
+from langgraph.graph import StateGraph, MessagesState, END
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage
+from langgraph.prebuilt import ToolNode, tools_condition
+import getpass
+from langgraph.checkpoint.memory import MemorySaver
 if not os.environ.get("OPENAI_API_KEY"):
   os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
 
@@ -72,50 +62,113 @@ print(len(document_ids))
 # api_url="https://api.smith.langchain.com" in hub.pull.
 prompt = hub.pull("rlm/rag-prompt")
 
-# Define state for application
-class State(TypedDict):
-    question: str
-    query: Search
-    context: List[Document]
-    answer: str
 
 
-def analyze_query(state: State):
-    structured_llm = llm.with_structured_output(Search)
-    query = structured_llm.invoke(state["question"])
-    return {"query": query}
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
 
 
-def retrieve(state: State):
-    query = state["query"]
-    retrieved_docs = vector_store.similarity_search(query["query"], filter=lambda doc: doc.metadata.get("section") == query["section"])
-    return {"context": retrieved_docs}
+# Step 1: Generate an AIMessage that may include a tool-call to be sent.
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = llm.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
 
 
-def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = llm.invoke(messages)
-    return {"answer": response.content}
+# Step 2: Execute the retrieval.
+tools = ToolNode([retrieve])
 
 
-graph_builder = StateGraph(State).add_sequence([analyze_query, retrieve, generate])
-graph_builder.add_edge(START, "analyze_query")
-graph = graph_builder.compile()
+# Step 3: Generate a response using the retrieved content.
+def generate(state: MessagesState):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Run
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
+
+
+graph_builder = StateGraph(MessagesState)
+
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+graph = graph_builder.compile(checkpointer=MemorySaver())
 # print(graph.get_graph().draw_mermaid())
 
-response = graph.invoke({"question": "What does the end of the post say about Task Decomposition?"})
-print(response["answer"])
+# Specify an ID for the thread
+config = {"configurable": {"thread_id": "abc123"}}
+input_message = "Hello"
 
-print("The updated query from LLM was: ", response["query"])
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+    config=config
+):
+    step["messages"][-1].pretty_print()
 
 
-# for step in graph.stream(
-#     {"question": "What is Task Decomposition?"}, stream_mode="updates"
-# ):
-#     print(f"{step}\n\n----------------\n")
+input_message = "What is Task Decomposition?"
 
-# for message, metadata in graph.stream(
-#     {"question": "What is Task Decomposition?"}, stream_mode="messages"
-# ):
-#     print(message.content, end="|")
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+    config=config
+):
+    step["messages"][-1].pretty_print()
+
+
+input_message = "Can you look up some common ways of doing it?"
+
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+    config=config,
+):
+    step["messages"][-1].pretty_print()
