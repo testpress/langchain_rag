531ba8bf3e84101659083a5009544f792c81c358
Bharathwaaj S <bharath@testpress.in>
2025-07-06 10:27:45 +0000

    Add main functionality for blog content processing and question-answering using LangChain. Implemented document loading, text splitting, and vector storage. Updated dependencies in pyproject.toml and added a lock file for package management.

--- main.py
+++ main.py
@@ -1,6 +1,121 @@
-def main():
-    print("Hello from langchain-rag!")
+from langchain.chat_models import init_chat_model
+import bs4
+import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
+from IPython.display import Image, display
+from typing import Literal
+
+from typing_extensions import Annotated
+
+class Search(TypedDict):
+    """Search query."""
+    query: Annotated[str, ..., "Search query to run."]
+    section: Annotated[
+        Literal["beginning", "middle", "end"],
+        ...,
+        "Section to query.",
+    ]
+
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+vector_store = InMemoryVectorStore(embeddings)
+
+# Load and chunk contents of the blog
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+assert len(docs) == 1
+print(f"Total characters: {len(docs[0].page_content)}")
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+
+print(f"Split blog post into {len(all_splits)} sub-documents.")
 
 
-if __name__ == "__main__":
-    main()
+total_documents = len(all_splits)
+third = total_documents // 3
+
+for i, document in enumerate(all_splits):
+    if i < third:
+        document.metadata["section"] = "beginning"
+    elif i < 2 * third:
+        document.metadata["section"] = "middle"
+    else:
+        document.metadata["section"] = "end"
+
+
+# Index chunks
+document_ids = vector_store.add_documents(documents=all_splits)
+
+print(document_ids[:3])
+print(len(document_ids))
+# Define prompt for question-answering
+# N.B. for non-US LangSmith endpoints, you may need to specify
+# api_url="https://api.smith.langchain.com" in hub.pull.
+prompt = hub.pull("rlm/rag-prompt")
+
+# Define state for application
+class State(TypedDict):
+    question: str
+    query: Search
+    context: List[Document]
+    answer: str
+
+
+def analyze_query(state: State):
+    structured_llm = llm.with_structured_output(Search)
+    query = structured_llm.invoke(state["question"])
+    return {"query": query}
+
+
+def retrieve(state: State):
+    query = state["query"]
+    retrieved_docs = vector_store.similarity_search(query["query"], filter=lambda doc: doc.metadata.get("section") == query["section"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+
+graph_builder = StateGraph(State).add_sequence([analyze_query, retrieve, generate])
+graph_builder.add_edge(START, "analyze_query")
+graph = graph_builder.compile()
+# print(graph.get_graph().draw_mermaid())
+
+response = graph.invoke({"question": "What does the end of the post say about Task Decomposition?"})
+print(response["answer"])
+
+print("The updated query from LLM was: ", response["query"])
+
+
+# for step in graph.stream(
+#     {"question": "What is Task Decomposition?"}, stream_mode="updates"
+# ):
+#     print(f"{step}\n\n----------------\n")
+
+# for message, metadata in graph.stream(
+#     {"question": "What is Task Decomposition?"}, stream_mode="messages"
+# ):
+#     print(message.content, end="|")
